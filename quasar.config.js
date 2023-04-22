8151559d4d7c299ad553a5580e22735c6e534568
RodrigoJuliano <srodrigo249@gmail.com>
2023-04-22 07:53:36 -0300

    Revert public path change to fix deploy on gh-pages

--- quasar.config.js
+++ quasar.config.js
@@ -65,7 +65,7 @@ module.exports = configure(function (ctx) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/filmox/',
+      publicPath: '/filmox/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
